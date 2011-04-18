e0d7240f0e3efc7d70157a3f9887c6c5dd79dfaf
Guy Royse <guy@guyroyse.com>
2011-04-18 05:21:19 -0400

    Implemented first gerkin for board

--- features/GameOfLife.feature
+++ features/GameOfLife.feature
@@ -3,3 +3,7 @@ Feature: Game of Life
 	Scenario: Cuke 4 Duke runs
 		When I add 2 plus 2
 		Then I get 4
+	Scenario: Starting game of life
+		Given that I want to play the game of life
+		When I start playing
+		Then there should be a board of size 100 by 100
--- features/step_definitions/GameOfLifeSteps.groovy
+++ features/step_definitions/GameOfLifeSteps.groovy
@@ -7,3 +7,12 @@ When(~/^I add (\d+) plus (\d+)$/) { int num1, int num2 ->
 Then(~/^I get (\d+)$/) { int arg1 ->
   assert arg1 == sum
 }
+
+Given(~/^that I want to play the game of life$/) {
+}
+
+When(~/^I start playing$/) {
+}
+
+Then(~/^there should be a board of size (\d+) by (\d+)$/) { int height, int width ->
+}
